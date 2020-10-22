495e49a3039532914645f08a0f61f7669a7bba13
Olli Havilehto <olli@havilehto.fi>
2020-10-22 18:40:17 +0300

    Enhance landing page on Safari browser

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ function App() {
   return (
     <div className={'App'}>
       <div id='container'>
-        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center', backgroundColor: '#5CDB95', color: '#EDF5E1', fontWeight: 'bold', fontSize: '50px', fontFamily: 'Abel' }}>
-          <div className={'avatarWrapper'}>
-            <img src={'/img/avatar.png'} alt='Avatar' className='avatar' />
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center', backgroundColor: '#5CDB95', color: '#EDF5E1', fontWeight: 'bold', fontSize: '45px', fontFamily: 'Abel' }}>
+          <div style={{ display: 'flex' }}>
+            <div className={'avatarWrapper'}>
+              <img src={'/img/avatar.png'} alt='Avatar' className='avatar' />
+            </div>
           </div>
-          <div style={{ display: 'flex', padding: '0 5px 0 5px', textShadow: '2px 2px black' }} >Hey, my name is Olli Havilehto</div>
-          <div style={{ display: 'flex', padding: '0 5px 0 5px', textShadow: '2px 2px black' }}>I do IT Architecture, programming, and DevOps stuff</div>
-          <div style={{ display: 'flex', padding: '0 5px 0 5px', textShadow: '2px 2px black' }}>~~~~~~</div>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', overflow: 'hidden' }}>
+            <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }} >Hey, my name is Olli Havilehto.</div>
+            <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }}>I do IT architecture, programming, and DevOps stuff.</div>
+            <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }}>~~~~~~</div>
+          </div>
         </div>
-        <div style={{ display: 'flex', color: 'white', width: '100%', justifyContent: 'center', backgroundColor: '#5CDB95' }}>
+        <div className={'SocialLinkWrapper'}>
           <SocialLink iconName="fab fa-linkedin" linkTarget={'https://www.linkedin.com/in/havilehto/'} tooltipText={'LinkedIn'} />
           <SocialLink iconName="fab fa-github" linkTarget={'https://github.com/olli1337'} tooltipText={'Github'} />
           <SocialLink iconName="fas fa-code" linkTarget={'https://leetcode.com/olli1337/'} tooltipText={'LeetCode'} />
