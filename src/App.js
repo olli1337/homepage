41d2bb810ab52ae0114781709da06b78e7dcb3ae
Olli Havilehto <olli@havilehto.fi>
2020-10-22 23:43:43 +0300

    Revamp the landing page, optimize for mobile use

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className={'App'}>
       <div id='container'>
-        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center', backgroundColor: '#5CDB95', color: '#EDF5E1', fontWeight: 'bold', fontSize: '45px', fontFamily: 'Abel' }}>
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center', backgroundColor: '#203647', color: '#EEFBFB', fontWeight: 'bold', fontSize: '45px', fontFamily: 'Abel', marginBottom: '20px' }}>
           <div style={{ display: 'flex' }}>
             <div className={'avatarWrapper'}>
               <img src={'/img/avatar.png'} alt='Avatar' className='avatar' />
@@ -15,7 +15,6 @@ function App() {
           <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', overflow: 'hidden' }}>
             <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }} >Hey, my name is Olli Havilehto.</div>
             <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }}>I do IT architecture, programming, and DevOps stuff.</div>
-            <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }}>~~~~~~</div>
           </div>
         </div>
         <div className={'SocialLinkWrapper'}>
@@ -23,7 +22,11 @@ function App() {
           <SocialLink iconName="fab fa-github" linkTarget={'https://github.com/olli1337'} tooltipText={'Github'} />
           <SocialLink iconName="fas fa-code" linkTarget={'https://leetcode.com/olli1337/'} tooltipText={'LeetCode'} />
           <SocialLink iconName="fab fa-google-play" linkTarget={'https://play.google.com/store/apps/developer?id=Havilehto+Technologies+Shaping+Your+Future&hl=fi'} tooltipText={'Google Play'} />
-          <SocialLink iconName="fas fa-desktop" linkTarget={'https://www.google.fi'} tooltipText={'Web apps'} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center', backgroundColor: '#203647', color: '#EEFBFB', fontWeight: 'bold', fontSize: '30px', fontFamily: 'Abel', marginTop: '20px'  }}>
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', overflow: 'hidden'}}>
+            <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }} >I have Master in Computer Science and I am Azure, Scrum, DevOps & ITIL certified.</div>
+          </div>
         </div>
       </div>
     </div>
@@ -31,3 +34,9 @@ function App() {
 }
 
 export default App;
+
+/*
+          <SocialLink iconName="fas fa-desktop" linkTarget={'https://www.udemy.com/user/olli-havilehto/'} tooltipText={'Udemy'} />
+                      <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }}>~~~~~~</div>
+
+*/
--- src/components/SocialLink/SocialLink.js
+++ src/components/SocialLink/SocialLink.js
@@ -4,11 +4,11 @@ import styles from './SocialLink.module.css';
 class SocialLink extends React.Component {
     render() {
         return (
-            <div className={styles.tooltip}>
-                <span className={styles.tooltipText}> {this.props.tooltipText} </span>
+            <div className={styles.socialLinkWrapper}>
                 <a className={styles.linkElement} href={this.props.linkTarget} target="_blank" rel="noopener noreferrer">
                     <i className={this.props.iconName}></i>
                 </a>
+                <span className={'linkElementTitle'}>{this.props.tooltipText}</span> 
             </div>
         )
     }
