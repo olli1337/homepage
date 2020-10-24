b62180d22e0aff81a3e139c8b093aaa3a499f411
Olli Havilehto <olli@havilehto.fi>
2020-10-25 00:50:03 +0300

    Remove unnecessary import

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import App from './App';
 import ReactDOM from 'react-dom';
-import { render, screen } from '@testing-library/react';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
