963eecd4b1275ae9db905f9b7764b89cb9eef91e
Olli Havilehto <olli@havilehto.fi>
2020-10-24 13:40:41 +0300

    Remove unnecessary comments, move landing page css away from inline

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import './App.css';
 import SocialLink from './components/SocialLink/SocialLink';
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className={'App'}>
-      <div id='container'>
-        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center', backgroundColor: '#203647', color: '#EEFBFB', fontWeight: 'bold', fontSize: '45px', fontFamily: 'Abel', marginBottom: '20px' }}>
-          <div style={{ display: 'flex' }}>
-            <div className={'avatarWrapper'}>
-              <img src={'/img/avatar.png'} alt='Avatar' className='avatar' />
-            </div>
+    <div className={styles.App}>
+      <div className={styles.container}>
+        <div className={styles.topSection}>
+          <div className={styles.avatarWrapper}>
+            <img src={'/img/avatar.png'} alt='Avatar' className='avatar' />
           </div>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', overflow: 'hidden' }}>
-            <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }} >Hey, my name is Olli Havilehto.</div>
-            <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }}>I do IT architecture, programming, and DevOps stuff.</div>
+          <div className={styles.topSectionTitle}>
+            <div className={styles.topSectionTitleText}>Hey, my name is Olli Havilehto.</div>
+            <div className={styles.topSectionTitleText}>I do IT architecture, programming, and DevOps stuff.</div>
           </div>
         </div>
-        <div className={'SocialLinkWrapper'}>
+        <div className={styles.SocialLinkWrapper}>
           <SocialLink iconName="fab fa-linkedin" linkTarget={'https://www.linkedin.com/in/havilehto/'} tooltipText={'LinkedIn'} />
           <SocialLink iconName="fab fa-github" linkTarget={'https://github.com/olli1337'} tooltipText={'Github'} />
           <SocialLink iconName="fas fa-code" linkTarget={'https://leetcode.com/olli1337/'} tooltipText={'LeetCode'} />
           <SocialLink iconName="fab fa-google-play" linkTarget={'https://play.google.com/store/apps/developer?id=Havilehto+Technologies+Shaping+Your+Future&hl=fi'} tooltipText={'Google Play'} />
         </div>
-        <div style={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center', backgroundColor: '#203647', color: '#EEFBFB', fontWeight: 'bold', fontSize: '30px', fontFamily: 'Abel', marginTop: '20px'  }}>
-          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', overflow: 'hidden'}}>
-            <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }} >I have Master in Computer Science and I am Azure, Scrum, DevOps & ITIL certified.</div>
+        <div className={styles.bottomSectionTitle}>
+          <div >
+            <div className={styles.bottomSectionTitleText}>I have Master in Computer Science and I am Azure, Scrum, DevOps & ITIL certified.</div>
           </div>
         </div>
       </div>
@@ -34,9 +33,3 @@ function App() {
 }
 
 export default App;
-
-/*
-          <SocialLink iconName="fas fa-desktop" linkTarget={'https://www.udemy.com/user/olli-havilehto/'} tooltipText={'Udemy'} />
-                      <div style={{ display: 'flex', margin: '0px 5px 0 5px', textShadow: '2px 2px black' }}>~~~~~~</div>
-
-*/
