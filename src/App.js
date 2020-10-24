687bbca71aec8302d7c85ee43a135cb9ef358e30
Olli Havilehto <olli@havilehto.fi>
2020-10-24 13:47:58 +0300

    Remove unused import, fix div name, fix classname

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import SocialLink from './components/SocialLink/SocialLink';
 import styles from './App.module.css'
 
@@ -9,7 +8,7 @@ function App() {
       <div className={styles.container}>
         <div className={styles.topSection}>
           <div className={styles.avatarWrapper}>
-            <img src={'/img/avatar.png'} alt='Avatar' className='avatar' />
+            <img src={'/img/avatar.png'} alt='Avatar' className={styles.avatar} />
           </div>
           <div className={styles.topSectionTitle}>
             <div className={styles.topSectionTitleText}>Hey, my name is Olli Havilehto.</div>
@@ -22,8 +21,8 @@ function App() {
           <SocialLink iconName="fas fa-code" linkTarget={'https://leetcode.com/olli1337/'} tooltipText={'LeetCode'} />
           <SocialLink iconName="fab fa-google-play" linkTarget={'https://play.google.com/store/apps/developer?id=Havilehto+Technologies+Shaping+Your+Future&hl=fi'} tooltipText={'Google Play'} />
         </div>
-        <div className={styles.bottomSectionTitle}>
-          <div >
+        <div className={styles.bottomSection}>
+          <div className={styles.bottomSectionTitle}>
             <div className={styles.bottomSectionTitleText}>I have Master in Computer Science and I am Azure, Scrum, DevOps & ITIL certified.</div>
           </div>
         </div>
