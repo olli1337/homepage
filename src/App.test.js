eca3444f892bc3d725a43f834d5155aabe952c1a
Olli Havilehto <olli@havilehto.fi>
2020-10-25 00:45:33 +0300

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import App from './App';
-import { shallow } from 'enzyme';
+import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 
-/*
-  Use shallow due to having TSX component which crashes Jest!
-*/
-
-it('Render App without crashing.', () => {
-  shallow(<App />);
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
-
-/*
-it('Landing page renders introduction successfully.', () => {
-  const wrapper = shallow(<App />);
-  const introduction = <SocialLink iconName="fab fa-linkedin" linkTarget={'https://www.linkedin.com/in/havilehto/'} tooltipText={'LinkedIn'} />;
-  expect(wrapper.contains(introduction)).toEqual(true);
-});
-*/
